146826c022219ab41b59c8f4c7c3bf7d03e51192
Kit Jasper M. Caballero <kcaballero_250000000510@uic.edu.ph>
2026-09-15 15:14:00 +0800

    modfied novalidate class in html and partial modification (WIP) on jscript

--- app.js
+++ app.js
@@ -4,6 +4,38 @@ const categoryNameInput = document.getElementById("txtCatName");
 const categoryDescInput = document.getElementById("txtCatDesc");
 const addCategoryBtn = document.getElementById("btnAdd");
 const incomeTableBody = document.getElementById("listIncomeCat");
+
+// added input elemts in the array for event handling (Kit modify start)
+const inputs = [categoryNameInput, categoryDescInput]; 
+
+// inline validation
+function validateField(input) {
+  if (input.value.trim() === "") {
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+    return false;
+  } else {
+    input.classList.remove("is-invalid");
+    input.classList.add("is-valid");
+    return true;
+  }
+}
+
+// Attached listeners
+inputs.forEach((input) => {
+  input.addEventListener("blur", () => validateField(input));
+
+  input.addEventListener("input", () => {
+    if (input.value.trim() !== "") {
+      input.classList.remove("is-invalid");
+      input.classList.add("is-valid");
+    }
+  });
+});
+// (Kit modify end)
+
+
+
 // 2. Attach Non-Inline Event Listener
 addCategoryBtn.addEventListener("click", handleAddCategory);
 // 3. Controller Action
